e07421523904ee9bc03d98748cdf2c7dba2aab5e
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2023-10-12 16:11:19 +0900

    Remove torch depends

--- ControlNet-v1-1-nightly/annotator/dwpose/__init__.py
+++ ControlNet-v1-1-nightly/annotator/dwpose/__init__.py
@@ -7,62 +7,62 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-import torch
-import numpy as np
-from . import util
-from .wholebody import Wholebody
+# import torch
+# import numpy as np
+# from . import util
+# from .wholebody import Wholebody
 
-def draw_pose(pose, H, W):
-    bodies = pose['bodies']
-    faces = pose['faces']
-    hands = pose['hands']
-    candidate = bodies['candidate']
-    subset = bodies['subset']
-    canvas = np.zeros(shape=(H, W, 3), dtype=np.uint8)
+# def draw_pose(pose, H, W):
+#     bodies = pose['bodies']
+#     faces = pose['faces']
+#     hands = pose['hands']
+#     candidate = bodies['candidate']
+#     subset = bodies['subset']
+#     canvas = np.zeros(shape=(H, W, 3), dtype=np.uint8)
 
-    canvas = util.draw_bodypose(canvas, candidate, subset)
+#     canvas = util.draw_bodypose(canvas, candidate, subset)
 
-    canvas = util.draw_handpose(canvas, hands)
+#     canvas = util.draw_handpose(canvas, hands)
 
-    canvas = util.draw_facepose(canvas, faces)
+#     canvas = util.draw_facepose(canvas, faces)
 
-    return canvas
+#     return canvas
 
 
-class DWposeDetector:
-    def __init__(self):
+# class DWposeDetector:
+#     def __init__(self):
 
-        self.pose_estimation = Wholebody()
+#         self.pose_estimation = Wholebody()
 
-    def __call__(self, oriImg):
-        oriImg = oriImg.copy()
-        H, W, C = oriImg.shape
-        with torch.no_grad():
-            candidate, subset = self.pose_estimation(oriImg)
-            nums, keys, locs = candidate.shape
-            candidate[..., 0] /= float(W)
-            candidate[..., 1] /= float(H)
-            body = candidate[:,:18].copy()
-            body = body.reshape(nums*18, locs)
-            score = subset[:,:18]
-            for i in range(len(score)):
-                for j in range(len(score[i])):
-                    if score[i][j] > 0.3:
-                        score[i][j] = int(18*i+j)
-                    else:
-                        score[i][j] = -1
+#     def __call__(self, oriImg):
+#         oriImg = oriImg.copy()
+#         H, W, C = oriImg.shape
+#         with torch.no_grad():
+#             candidate, subset = self.pose_estimation(oriImg)
+#             nums, keys, locs = candidate.shape
+#             candidate[..., 0] /= float(W)
+#             candidate[..., 1] /= float(H)
+#             body = candidate[:,:18].copy()
+#             body = body.reshape(nums*18, locs)
+#             score = subset[:,:18]
+#             for i in range(len(score)):
+#                 for j in range(len(score[i])):
+#                     if score[i][j] > 0.3:
+#                         score[i][j] = int(18*i+j)
+#                     else:
+#                         score[i][j] = -1
 
-            un_visible = subset<0.3
-            candidate[un_visible] = -1
+#             un_visible = subset<0.3
+#             candidate[un_visible] = -1
 
-            foot = candidate[:,18:24]
+#             foot = candidate[:,18:24]
 
-            faces = candidate[:,24:92]
+#             faces = candidate[:,24:92]
 
-            hands = candidate[:,92:113]
-            hands = np.vstack([hands, candidate[:,113:]])
+#             hands = candidate[:,92:113]
+#             hands = np.vstack([hands, candidate[:,113:]])
             
-            bodies = dict(candidate=body, subset=score)
-            pose = dict(bodies=bodies, hands=hands, faces=faces)
+#             bodies = dict(candidate=body, subset=score)
+#             pose = dict(bodies=bodies, hands=hands, faces=faces)
 
-            return draw_pose(pose, H, W)
+#             return draw_pose(pose, H, W)
